d40778736ab8a4784ea40ae3fa52346291fb6d0c
Michael Muchiri <muchirimichael2222@gmail.com>
2023-10-11 11:01:28 +0300

    Added link to logo

---
+++ index.js
@@ -0,0 +1,8 @@
+//Adding link to API Documentation to the logo
+const logo=document.querySelector('#logo');
+document.addEventListener('DOMContentLoaded',()=>{
+    const a=document.createElement('a')
+    a.href='https://soruly.github.io/trace.moe-api/#/docs'
+    document.querySelector('#first-div').appendChild(a)
+    a.appendChild(logo)
+})

60ecf82bbad4daa8259bfe53d260e2653f64e571
Michael Muchiri <muchirimichael2222@gmail.com>
2023-10-11 12:38:19 +0300

    Updated css and html files

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 //Adding link to API Documentation to the logo
 const logo=document.querySelector('#logo');
 document.addEventListener('DOMContentLoaded',()=>{
-    const a=document.createElement('a')
-    a.href='https://soruly.github.io/trace.moe-api/#/docs'
-    document.querySelector('#first-div').appendChild(a)
-    a.appendChild(logo)
+    const a=document.createElement('a');
+    a.href='https://soruly.github.io/trace.moe-api/#/docs';
+    document.querySelector('#first-div').appendChild(a);
+    a.appendChild(logo);
 })
 
 //Adding a submit event listener to the form and performing a fetch
 //request.
-const form=document.querySelector('')
+const form=document.querySelector('#url-form');
+form.addEventListener('submit',e=>{
+    e.preventDefault();
+    const input=document.querySelector('#img-url');
+
+    fetch(`https://api.trace.moe/search?url=${input.value}`)
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data)
+        const video=document.querySelector('#video-result')
+        const ul=document.querySelector('.info')
+        const arr=data.result
+
+        video.src=arr[0].video
+    })
+})
